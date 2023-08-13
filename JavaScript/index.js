cc5d817392061c3b069ba36f19e670b325d47709
MatkovskyA <a.matkovsky14@gmail.com>
2023-08-13 18:08:53 +0300

    Подключен слайдер slick

---
+++ JavaScript/index.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        // arrows:false, по макету их не видно
+        dots:true,
+    });
+});
+
+
+
+
+
+// Самостоятельный вариант слайдера(не доработан)
+
+// // константы из index.html
+// const sliderLineNode = document.querySelector('.slider__wrapper');
+// const prevSliderBtn = document.querySelector('.btn__prev_slide');
+// const nextSliderBtn = document.querySelector('.btn__next_slide');
+// const dotsNode = document.querySelectorAll('.dot');
+
+
+// let position = 0;
+// // для выбора индикатора под слайдами
+// let dotIndex = 0;
+
+// //-----functions---------
+
+// const nextSlide = () => {
+//     if (position < 2880) {
+//     position += 960;
+//     } else {
+//         position = 0;
+//     }
+//     sliderLineNode.style.left = -position + 'px';
+// }
+
+// // ----eventlisteners----
+
+// nextSliderBtn.addEventListener('click', nextSlide);
+
+// const showMes = () => {
+//     const name = true;
+//     if (name) {
+//         alert("Ok")
+//     }
+// }
+
+// prevSliderBtn.addEventListener('click', showMes)
+
+
