b888152fb2db49230e41dd550f39f5e15bf7fb0b
MatkovskyA <a.matkovsky14@gmail.com>
2023-08-14 15:38:29 +0300

    Footwear - отображение текста по событию

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,6 +6,38 @@ $(document).ready(function(){
 });
 
 
+// --------- отображаем текст в блоке footwears ----------
+const btnShowBar = document.getElementById('show_bar');
+const btnBlockBar = document.getElementById('block_bar');
+let viewTextDiv = document.getElementById("footwear__inner_text");
+viewTextDiv.style.display = 'none';
+
+const header = document.getElementById("footwear__dots");
+const btns = header.getElementsByClassName("dots3");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+//функции
+function showText() {
+    viewTextDiv.style.display = 'inline';
+}
+
+function blockText() {
+    viewTextDiv.style.display = 'none';
+}
+//обработчики
+btnShowBar.addEventListener('click', showText)
+btnBlockBar.addEventListener('click', blockText)
+
+
+
+
 
 
 
